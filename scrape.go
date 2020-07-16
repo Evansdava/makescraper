e24548d883b4e47333729ca977a58550e10d5ffe
David Evans <evansdav.a@gmail.com>
2020-07-16 11:49:12 -0700

    Finished requirements

--- scrape.go
+++ scrape.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
+
+type comment struct {
+	Author string
+	Time   string
+	Text   string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -12,12 +20,27 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	// Find the latest page of the comic (and not other posts)
+	c.OnHTML(".postonpage-1 .comment-link a", func(e *colly.HTMLElement) {
+		// Navigate to page
+		e.Request.Visit(e.Attr("href"))
+	})
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// On every top level element
+	c.OnHTML(".comment.depth-1 .comment-content", func(e *colly.HTMLElement) {
+		// print comment text
+		comm := comment{e.ChildText(".comment-author cite"), e.ChildText(".comment-time"), e.ChildText(".comment-text p")}
+
+		commJson, err := json.MarshalIndent(comm, "", "  ")
+		checkErr(err)
+		fmt.Println(string(commJson))
+
+		writeJson(commJson, os.O_APPEND)
+
+		fmt.Println("Author: ", comm.Author)
+		fmt.Println("Time: ", comm.Time)
+		fmt.Println("Comment: ", comm.Text)
+		fmt.Println()
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +48,27 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Print finished after everything has been scraped
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Finished", r.Request.URL)
+	})
+
+	// Start scraping on https://killsixbilliondemons.com
+	c.Visit("https://killsixbilliondemons.com/")
+}
+
+func writeJson(data []byte, flag int) {
+	f, err := os.OpenFile("output.json", flag, 0644)
+	checkErr(err)
+	defer f.Close()
+
+	n, err := f.Write(data)
+	checkErr(err)
+	fmt.Printf("Wrote %d bytes to %s\n", n, f.Name())
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
